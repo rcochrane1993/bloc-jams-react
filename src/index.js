efe534dcc833627c8154f560bfeb48f00db6a0c8
Robert Cochrane <rcochrane1105@gmail.com>
2018-04-09 18:43:15 -0400

    assignment 12 bloc jams routing completed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-=======
 import { BrowserRouter }from 'react-router-dom';
->>>>>>> checkpoint-12-bloc-jams-routing
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
   , document.getElementById('root'));
->>>>>>> checkpoint-12-bloc-jams-routing
 registerServiceWorker();
