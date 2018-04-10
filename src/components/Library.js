91bce338de615f2afb70996a6d291b19372cb7a8
Robert Cochrane <rcochrane1105@gmail.com>
2018-04-10 12:24:11 -0400

    working on checkpoint 14

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import albumData from './src/data/albums'
 
-const Library = () => (
-  <section className="library">
-    Library will go here
-  </section>
-);
+class Library extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {albums: albumData };
+    }
+   render() {
+    return (
+      <section className='library'>
+        {
+          this.state.albums.map((album,index) =>
+            <div key={index} >
+              {album.title}
+            </div>
+          )
+        }
+      </section>
+     );
+   }
+ }
 
 export default Library;
