18125d7129d6ef1f2565a6b09c2d4cd909efb4ff
Robert Cochrane <rcochrane1105@gmail.com>
2018-04-10 17:36:11 -0400

    checkpoint 16 bloc jams audio playback completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,9 +14,39 @@ class Album extends Component {
 
      this.state = {
        album: album,
-       songs: songs
-     }
+       songs: songs,
+       currentSong: album.songs[0],
+       isPlaying: false
+     };
+
+     this.audioElement = document.createElement('audio');
+     this.audioElement.src = album.songs[0].audioSrc;
   }
+
+  play(){
+    this.audioElement.play();
+    this.setState({isPlaying: true});
+  }
+
+  pause(){
+    this.audioElement.pause();
+    this.setState({ isPlaying: false});
+  }
+
+  setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+   }
+
+   handleSongClick(song){
+     const isSameSong = this.state.currentSong === song;
+     if (this.state.isPlaying && isSameSong) {
+       this.pause();
+     } else {
+       if (!isSameSong){this.setSong(song); }
+       this.play();
+     }
+   }
    render() {
      return (
        <section className="album">
@@ -37,7 +67,7 @@ class Album extends Component {
         <tbody>
           {
             this.state.album.songs.map((song,index)=>
-            <tr className="song-details" key={index}>
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
               <td className="song-actions">
                  <button>
                    <span className="song-number">{index+1}</span>
