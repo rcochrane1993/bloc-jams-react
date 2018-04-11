c90c28f8acc3261fdb6b975461529cdfc2290110
Robert Cochrane <rcochrane1105@gmail.com>
2018-04-11 00:29:19 -0400

    assignment 17 bloc jams playerbar buttons completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -52,6 +52,15 @@ class Album extends Component {
       this.setSong(newSong);
       this.play(newSong);
    }
+
+   handleNextClick(){
+     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song );
+     const newIndex = Math.min(4, currentIndex + 1);
+     const newSong = this.state.album.songs[newIndex];
+     this.setSong(newSong);
+     this.play(newSong);
+   }
+
    render() {
      return (
        <section className="album">
@@ -89,6 +98,7 @@ class Album extends Component {
          currentSong={this.state.currentSong}
          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
          handlePrevClick={() => this.handlePrevClick()}
+         handleNextClick={() => this.handleNextClick()}
        />
       </section>
      );
